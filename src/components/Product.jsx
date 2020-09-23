933266b74d7c3177c715501068e547bd7229ffae
ADILZAMAL <adilzamal@gmail.com>
2020-09-23 17:22:39 +0530

    cart component completed

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,14 +4,27 @@ import "../style/Product.css";
 export default function Product(props) {
   const { _id, image, title, description, availableSize, price } = props.data;
   return (
-    <Col className="product p-5" sm="6" md="4">
-      <div className="product__image">
+    <Col className="product p-5 border" sm="6" md="4">
+      <div className="product__image ">
         <img src={image} alt={title} />
       </div>
       <h2 className="product__title">{title}</h2>
       <div className="product__btn_price">
         <h1 className="product__price">${price}</h1>
-        <Button size="sm" className="product__btn">
+        <Button
+          onClick={() => {
+            props.addToCartItem({
+              _id,
+              image,
+              title,
+              description,
+              availableSize,
+              price,
+            });
+          }}
+          size="sm"
+          className="product__btn"
+        >
           Add to cart
         </Button>
       </div>
